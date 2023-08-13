130ccc59882486fc28ee595d6cf9ca64278a8f1a
mfathur <mfathur17@gmail.com>
2023-08-13 22:37:22 +0700

    add profile component for home page

--- src/components/home/profile.components.tsx
+++ src/components/home/profile.components.tsx
@@ -1,11 +1,30 @@
+import { useNavigate } from "react-router-dom";
 import PopupMenu from "./popup-menu.component";
+import useAuthContext from "hooks/useAuthContext";
 
 type Props = {};
 
 const Profile = (props: Props) => {
+  const { user } = useAuthContext();
+  const userLoggedIn = user !== null;
+  const navigate = useNavigate();
+
+  const handleTxtLoginClick = () => {
+    navigate("/auth/login");
+  };
+
   return (
     <div className="flex items-center">
-      <p className="font-bold text-lg">Hi, trial</p>
+      {userLoggedIn ? (
+        <p className="font-bold text-lg">Hi, {user?.username}</p>
+      ) : (
+        <p
+          className="font-bold text-lg hover:cursor-pointer hover:text-gray-300"
+          onClick={handleTxtLoginClick}
+        >
+          Login
+        </p>
+      )}
       <PopupMenu />
     </div>
   );
