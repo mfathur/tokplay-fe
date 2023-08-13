5626c73b2cbdacb8d2da475b34ea9631a6eebc23
mfathur <mfathur17@gmail.com>
2023-08-13 22:36:06 +0700

    add video component for home page

--- src/components/home/video-item.component.tsx
+++ src/components/home/video-item.component.tsx
@@ -1,15 +1,30 @@
-type Props = {};
+import { useNavigate } from "react-router-dom";
 
-const VideoItem = (props: Props) => {
+type Props = {
+  id: string;
+  imgSrc: string;
+  title: string;
+  merchant: string;
+  link: string;
+};
+
+const VideoItem = ({ id, imgSrc, title, merchant, link }: Props) => {
+  const navigate = useNavigate();
+
+  const goToVideoDetailPage = () => {
+    navigate(`videos/${id}`, {
+      state: { title, merchant, link },
+    });
+  };
+
   return (
-    <div className="relative rounded-lg bg-red-200 w-1/5 h-96 hover:cursor-pointer">
-      <img
-        className="rounded-lg bg-cover h-96"
-        src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80"
-        alt=""
-      />
-      <p className="font-bold absolute bottom-8 left-2">Video title</p>
-      <p className="absolute left-2 bottom-2 text-sm">Merchant</p>
+    <div
+      onClick={goToVideoDetailPage}
+      className="relative rounded-lg bg-red-200 w-1/5 h-96 hover:cursor-pointer"
+    >
+      <img className="rounded-lg bg-cover h-96" src={imgSrc} alt={title} />
+      <p className="font-bold absolute bottom-8 left-2">{title}</p>
+      <p className="absolute left-2 bottom-2 text-sm">{merchant}</p>
     </div>
   );
 };
--- src/components/home/videos-section.component.tsx
+++ src/components/home/videos-section.component.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import Video from "types/video";
 import VideoItem from "./video-item.component";
 
-type Props = {};
+type Props = {
+  videos: Video[] | null;
+};
 
-const VideosSection = (props: Props) => {
+const VideosSection = ({ videos }: Props) => {
   return (
     <div className="flex mt-6 grow gap-4">
-      <VideoItem />
-      <VideoItem />
-      <VideoItem />
-      <VideoItem />
-      <VideoItem />
+      {videos?.map((video) => (
+        <VideoItem
+          key={video._id}
+          id={video._id}
+          title={video.title}
+          merchant={video.merchant}
+          imgSrc={video.thumbnail}
+          link={video.link}
+        />
+      ))}
     </div>
   );
 };
