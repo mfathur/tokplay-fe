44dc4cbfb4bcf0de4fddfb6a35298b065076df0f
mfathur <mfathur17@gmail.com>
2023-08-13 22:27:28 +0700

    implement logic get data in home page

--- src/components/home/tf-search.component.tsx
+++ src/components/home/tf-search.component.tsx
@@ -1,15 +1,21 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-type Props = {};
+type Props = {
+  className?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+};
 
-const SearchTextField = (props: Props) => {
+const SearchTextField = ({ className, value, onChange }: Props) => {
   return (
-    <div className="flex grow relative">
+    <div className={`${className} flex grow relative`}>
       <input
         className="bg-darkmineral rounded-lg py-2 grow mr-5 pl-9 pr-6 font-medium"
         type="text"
         placeholder="Search videos..."
+        value={value}
+        onChange={onChange}
       />
       <FontAwesomeIcon
         className="absolute inset-y-3 left-3 hover:cursor-pointer"
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,51 @@
 import Profile from "components/home/profile.components";
 import SearchTextField from "components/home/tf-search.component";
 import VideosSection from "components/home/videos-section.component";
+import Spinner from "components/spinner.component";
+import { useAxiosGet } from "hooks/useAxiosGet";
+import { useEffect, useState } from "react";
+import Video from "types/video";
 
-type Props = {};
+const HomePage = () => {
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const [debouncedSearchKeyword, setDebouncedSearchKeyword] = useState<
+    string | null
+  >(null);
 
-const HomePage = (props: Props) => {
+  const {
+    data: videos,
+    error,
+    refetch,
+  } = useAxiosGet<Video[]>("/videos", {
+    params: { keyword: debouncedSearchKeyword },
+  });
+
+  useEffect(() => {
+    refetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearchKeyword]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedSearchKeyword(searchKeyword);
+    }, 600);
+
+    return () => clearTimeout(timerId);
+  }, [searchKeyword]);
+
   return (
-    <div className="flex flex-col min-h-screen my-6 mx-9">
+    <div className="flex flex-col min-h-screen py-6 px-9">
       <div className="flex items-center justify-between">
-        <SearchTextField />
+        <SearchTextField
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
         <Profile />
       </div>
-      <VideosSection />
+      {!videos ? (
+        <Spinner className="text-gray-500 self-center my-auto loading-lg" />
+      ) : null}
+      {videos && !error ? <VideosSection videos={videos} /> : null}
     </div>
   );
 };
